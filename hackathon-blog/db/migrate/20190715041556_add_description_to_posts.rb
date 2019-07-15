6a2f07287c8f1435fa6b5d4a47f60ebf9eb4fd57
raeginez <rachelginez@yahoo.com>
2019-07-15 14:51:20 +1000

    added devise, post model, post controller, input dummy data, modified the homepage

---
+++ hackathon-blog/db/migrate/20190715041556_add_description_to_posts.rb
@@ -0,0 +1,6 @@
+class AddDescriptionToPosts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :description, :string
+    
+  end
+end
--- hackathon-blog/db/seeds.rb
+++ hackathon-blog/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+User.destroy_all
+
+rachelgonzales = User.create(username: "rg", email: "rachelgonzales@gmail.com", password: "123456")
+rachelgonza = User.create(username: "rrg", email: "rachelgonza@gmail.com", password: "1234567")
+rachelgonzales.posts.create(title: "first Blog", description: "i love blogging", user_id: rachelgonzales.id)
+rachelgonza.posts.create(title: "second blog", description: "second blog", user_id: rachelgonza.id)
