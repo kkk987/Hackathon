c4c567bdbc7de127bc2f4cc269cadb350aab50dc
kkk987 <markying21549@gmail.com>
2019-07-16 11:57:36 +1000

    Added features:
    - User can view each post inn detail
    - user can edit and delete his/her post

--- hackathon-blog/app/controllers/posts_controller.rb
+++ hackathon-blog/app/controllers/posts_controller.rb
@@ -17,6 +17,36 @@ class PostsController < ApplicationController
     def show
     
     end
+    
+    def display
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        # post_id = params[:id]
+        Post.find(params[:id]).destroy
+        redirect_to posts_show_path
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        post = Post.find(params[:id])
+        updated_description = params[:post]
+        if (post.title != updated_description[:title]) || (post.description != updated_description[:description])
+            post.title = updated_description[:title]
+            post.description = updated_description[:description]
+        end
+        # byebug
+        if post.save
+            flash[:alert] = "Your changes have been saved"
+        else
+            flash[:alert] = "There was an error with your change"
+        end
+        redirect_to post_path(post.id)
+    end
 
 
 end
--- hackathon-blog/config/routes.rb
+++ hackathon-blog/config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "posts/new", to: "posts#new"
   post "posts", to: "posts#create"
   get "posts/show", to: "posts#show"
-
+  get "posts/:id", to: "posts#display", as: "post"
+  get "posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "posts/:id", to: "posts#update", as: "update_post"
+  delete "posts/:id", to: "posts#destroy", as: "delete_post"
 end
